fc961f9caf8d33ffbc159393677076364d692208
zjpahle <zjpahle@users.noreply.github.com>
2014-06-17 14:32:59 -0700

    hue binarizing fixed around orange

--- vision2.py
+++ vision2.py
@@ -23,17 +23,22 @@ def video():
 def test_stream():
 	while(cap.isOpened()):
 		ret, frame = cap.read()
+		
 		if ret == True:
-			ORANGE_MIN = np.array([5, 50, 50],np.uint8)
-			ORANGE_MAX = np.array([15, 255, 255],np.uint8)
-			hsv_img = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-			frame = cv2.inRange(hsv_img, ORANGE_MIN, ORANGE_MAX)
+			frame = cv2.GaussianBlur(frame, (5,5), 0)
+			hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+			orange_min = np.array([5, 100, 100],np.uint8)
+			orange_max = np.array([10, 255, 255],np.uint8)
+			
+			mask = cv2.inRange(hsv_img, orange_min, orange_max)
 			kernel = np.ones((5,5),np.uint8)
-			opening = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
-			closing = cv2.morphologyEx(frame, cv2.MORPH_CLOSE, kernel)
-			#contours, hierarchy = cv2.findContours(closing,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-			#cv2.drawContours(frame,contours,1,(0,255,0),3)
-			cv2.imshow('frame',closing)
+			opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+			closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+			closing, contours, hierarchy = cv2.findContours(closing,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+			#cv2.drawContours(closing,contours,1,(0,255,0),3)
+			#cv2.imshow('frame',frame)
+			cv2.imshow('hue',mask)
+			cv2.imshow('mask',closing)
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
 	cap.release()
